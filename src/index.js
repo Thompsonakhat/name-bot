22a7da0d73ac2e514fffaedc0733edc5c8b00c9a
Your Name <you@example.com>
2025-10-09 02:09:10 +0100

    feat: AI edit OK

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const { Bot } = require('grammy');
 const bot = new Bot(process.env.BOT_TOKEN);
 
-const helpCommand = require('./commands/help');
 const startCommand = require('./commands/start');
+const helpCommand = require('./commands/help');
 const nameCommand = require('./commands/name');
 const usernameCommand = require('./commands/username');
 
-bot.command('help', helpCommand);
 bot.command('start', startCommand);
+bot.command('help', helpCommand);
 bot.command('name', nameCommand);
 bot.command('username', usernameCommand);
 
