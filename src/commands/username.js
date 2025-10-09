a3f1d26e8cf397dcb818c20d5e3865312519365d
Your Name <you@example.com>
2025-10-09 02:06:38 +0100

    chore: pre-AI-edit snapshot

--- src/commands/username.js
+++ src/commands/username.js
@@ -4,9 +4,3 @@ const usernameCommand = (ctx) => {
 };
 
 module.exports = { usernameCommand };
-const usernameCommand = (ctx) => {
-  const username = ctx.from.username ? ctx.from.username : 'No username';
-  ctx.reply(`Your username is: ${username}`);
-};
-
-module.exports = { usernameCommand };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-const { startCommand } = require('./commands/start');
-const { helpCommand } = require('./commands/help');
-const { nameCommand } = require('./commands/name');
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const helpCommand = require('./commands/help');
+const startCommand = require('./commands/start');
+const nameCommand = require('./commands/name');
+const usernameCommand = require('./commands/username');
+
+bot.start(startCommand);
+bot.help(helpCommand);
+bot.command('name', nameCommand);
+bot.command('username', usernameCommand);
+
+bot.launch();
+
+console.log('Bot is running...');
+
 const { usernameCommand } = require('./commands/username');
 
-const commands = {
-  start: startCommand,
-  help: helpCommand,
-  name: nameCommand,
-  username: usernameCommand
-};
-
-module.exports = commands;
+bot.command('username', usernameCommand);
